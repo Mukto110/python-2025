ef8117e46e4ee8b8cf7b80b759b5cd08ea00c02a
Meraj Hossain Mukto <merajhossainmukto@gmail.com>
2025-10-26 16:18:50 +0600

    complete day 1

---
+++ day1.py
@@ -0,0 +1,18 @@
+print("Mukto")
+
+server_name = "QA-Server-01"
+uptime = 48.5
+is_active = True
+
+print(server_name, uptime, is_active)
+
+test_suit = input("Enter test suite name: ")
+print(f"Running test suite: {test_suit}")
+
+tester_name = input(f"Tester: ")
+total_test_cases = input(f"Total Test Cases: ")
+status = input(f"Status: ")
+
+print(tester_name)
+print(total_test_cases)
+print(status)
